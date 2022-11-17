7da224b6802cf60dd9d504577bf928e7c478b92e
Waylon Jepsen <waylonjepsen1@gmail.com>
2022-11-17 10:20:00 -0700

    Tokens
    - constructed token objects for eth and USDC to test getpoolID

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,10 +1,11 @@
 use bindings::{i_uniswap_v3_pool::IUniswapV3Pool, meta_stable_pool::MetaStablePool, vault::Vault};
+use ethers::abi::Token;
 use ethers::prelude::*;
 use ethers::providers::Provider;
 use eyre::Result;
 use std::convert::TryFrom;
 use std::sync::Arc;
-// mod utils;
+mod tokens;
 
 // ETH: 0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2
 // USDC: 0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48
@@ -26,9 +27,14 @@ async fn main() -> Result<()> {
     // The vault is how we execute swaps on balancer
     let balancer_vault = Vault::new(balancer_vault_address, provider.clone());
 
+    let tokens = tokens::get_tokens();
     println!("Contract Addresses");
     println!("Balancer Vault Address: {:#?}", balancer_vault);
     println!("UniswapV3 Factory Address: {:#?}", uniswap_factory);
 
+    // pub fn get_pool_id(token1: Token, token2: Token) {
+    //     return;
+    // }
+    // uniswap_factory.get_pool(pool_id)
     Ok(())
 }
