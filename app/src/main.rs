ace12f9f7379e04da48bfb0829699ce9a90ce1e3
Colin Roberts <colin@primitive.xyz>
2022-11-19 07:43:25 -0700

    push for now, error in pricing

--- app/src/main.rs
+++ app/src/main.rs
@@ -8,21 +8,22 @@ async fn main() -> Result<()> {
     // Input CLI arguments from user
     let (token_0_string, token_1_string, _api_key) = cli::get_cli();
 
-    // Sync through Alchemy
+    // Sync to the chain through Alchemy
     let provider = utils::get_provider().await;
     let uniswap_factory = utils::get_uniswapv3_factory(provider.clone()).await;
     let tokens = tokens::get_tokens();
 
-    // BP 10000, 3000, 500, 100
+    // Return addresses of UniswapV3 pools given a token pair
+    // If 0x00...00 returns, the pool does not exist!
     let result_address = utils::get_pool_from_uniswap(
-        tokens.get(token_0.as_str()).unwrap().address,
-        tokens.get(token_1.as_str()).unwrap().address,
+        tokens.get(token_0_string.as_str()).unwrap().address,
+        tokens.get(token_1_string.as_str()).unwrap().address,
         uniswap_factory,
     )
     .await;
     println!("Uniswap Pool Result: {:#?}", result_address);
 
-    // TODO Change the result address to not always take the first indicy but all pools
+    // TODO: Change the result address to not always take the first index but all pools
     let pool_objects = utils::get_pool_objects(result_address, provider).await;
     utils::monitor_pool(&pool_objects[1], token_0_string, token_1_string).await;
 
--- app/src/utils/mod.rs
+++ app/src/utils/mod.rs
@@ -8,11 +8,6 @@ use std::sync::Arc;
 
 use crate::tokens::{self, get_tokens, Token};
 
-// Define constants necessary for pricing using BigFloat
-const two: BigFloat = 2.0.into();
-const ten: BigFloat = 10.0.into();
-const one: BigFloat = 1.into();
-
 pub async fn get_provider() -> Arc<Provider<Http>> {
     let provider =
         Provider::try_from("https://eth-mainnet.g.alchemy.com/v2/I93POQk49QE9O-NuOz7nj7sbiluW76it")
@@ -32,12 +27,12 @@ pub async fn get_pool_from_uniswap(
     token_1: Address,
     factory: UniswapV3Factory<Provider<Http>>,
 ) -> Vec<Address> {
-    let pool_500 = factory
+    let pool_100 = factory
         .get_pool(token_0, token_1, 100)
         .call()
         .await
         .unwrap(); // The UniswapV3 1BP pool.
-    let pool_100 = factory
+    let pool_500 = factory
         .get_pool(token_0, token_1, 500)
         .call()
         .await
@@ -51,7 +46,7 @@ pub async fn get_pool_from_uniswap(
         .get_pool(token_0, token_1, 10000)
         .call()
         .await
-        .unwrap();  // The UniswapV3 100BP pool.
+        .unwrap(); // The UniswapV3 100BP pool.
     vec![pool_100, pool_500, pool_3000, pool_10000]
 }
 pub async fn get_pool_objects(
@@ -107,13 +102,13 @@ pub async fn monitor_pool(
         // Takes in UniswapV3's sqrt_price_x96 (a q64_96 fixed point number) and outputs the price in human readable form.
         // See Uniswap's documentation: https://docs.uniswap.org/sdk/guides/fetching-prices
         let tokens = get_tokens();
-        let diff_decimals: BigFloat = (tokens.get(token_0_str).unwrap().base_units
-            - tokens.get(token_1_str).unwrap().base_units)
+        let diff_decimals: BigFloat = ((tokens.get(token_0_str).unwrap().base_units as i16)
+            - (tokens.get(token_1_str).unwrap().base_units) as i16)
             .into();
-        one.div(
+        BigFloat::from_i16(1).div(
             &&convert_q64_96(sqrt_price_x96)
-                .pow(&two)
-                .div(&ten.pow(&diff_decimals)),
+                .pow(&BigFloat::from_i16(2))
+                .div(&BigFloat::from_i16(10).pow(&diff_decimals)),
         )
     }
 
@@ -123,13 +118,13 @@ pub async fn monitor_pool(
         let second_sig = q64_96.0[1];
         let third_sig = q64_96.0[2];
         let most_sig = q64_96.0[3];
-    
+
         let bf2 = BigFloat::from(2);
         let bf64 = BigFloat::from(64);
         let bf128 = BigFloat::from(128);
         let bf192 = BigFloat::from(192);
         let bf96 = BigFloat::from(96);
-    
+
         ((BigFloat::from(most_sig) * bf2.pow(&bf192))
             + (BigFloat::from(third_sig) * bf2.pow(&bf128))
             + (BigFloat::from(second_sig) * bf2.pow(&bf64))
