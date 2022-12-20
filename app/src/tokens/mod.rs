7ff4cbdfd6b369fd2302fc3660076f5295d792f8
ts0yu <ts0yu@proton.me>
2022-12-20 21:58:20 +0000

    token naming convention fix

--- app/src/tokens/mod.rs
+++ app/src/tokens/mod.rs
@@ -50,23 +50,21 @@ pub fn get_tokens() -> HashMap<String, Token> {
     let dai = Token::new(dai_address, 18, String::from("DAI"), true);
 
     // USDT https://tether.to/
-    let tether_address = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
+    let usdt_address = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
         .parse::<Address>()
         .unwrap();
-    let tether = Token::new(tether_address, 6, String::from("USDT"), true);
+    let usdt = Token::new(usdt_address, 6, String::from("USDT"), true);
 
     // USDC https://www.centre.io/
     let usdc_address = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
         .parse::<Address>()
         .unwrap();
-
     let usdc = Token::new(usdc_address, 6, String::from("USDC"), true);
 
     // FRAX https://frax.finance/
     let frax_address = "0x853d955aCEf822Db058eb8505911ED77F175b99e"
         .parse::<Address>()
         .unwrap();
-
     let frax = Token::new(frax_address, 18, String::from("FRAX"), true);
 
     // Chainlink
@@ -74,21 +72,18 @@ pub fn get_tokens() -> HashMap<String, Token> {
     let link_address = "0x853d955aCEf822Db058eb8505911ED77F175b99e"
         .parse::<Address>()
         .unwrap();
-
     let link = Token::new(link_address, 18, String::from("LINK"), false);
 
     // stETH2 https://app.stakewise.io/
     let s_eth2_address = "0xFe2e637202056d30016725477c5da089Ab0A043A"
         .parse::<Address>()
         .unwrap();
-
     let s_eth2 = Token::new(s_eth2_address, 18, String::from("sETH2"), false);
 
     // USDM https://www.mappedswap.io/
     let usdm_address = "0xbbAec992fc2d637151dAF40451f160bF85f3C8C1"
         .parse::<Address>()
         .unwrap();
-
     let usdm = Token::new(usdm_address, 6, String::from("USDM"), true);
 
     // Binance US Dollar
@@ -96,7 +91,6 @@ pub fn get_tokens() -> HashMap<String, Token> {
     let busd_address = "0x4Fabb145d64652a948d72533023f6E7A623C7C53"
         .parse::<Address>()
         .unwrap();
-
     let busd = Token::new(busd_address, 18, String::from("USDM"), true);
 
     // Uniswap
@@ -104,14 +98,12 @@ pub fn get_tokens() -> HashMap<String, Token> {
     let uni_address = "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984"
         .parse::<Address>()
         .unwrap();
-
     let uni = Token::new(uni_address, 18, String::from("UNI"), false);
 
     // BTT https://www.bittorrent.com/
     let btt_address = "0xC669928185DbCE49d2230CC9B0979BE6DC797957"
         .parse::<Address>()
         .unwrap();
-
     let btt = Token::new(btt_address, 18, String::from("BTT"), false);
 
     // Wrapped Staked Ethereum
@@ -119,7 +111,6 @@ pub fn get_tokens() -> HashMap<String, Token> {
     let wsteth_address = "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0"
         .parse::<Address>()
         .unwrap();
-
     let wsteth = Token::new(wsteth_address, 18, String::from("WSTETH"), false);
 
     // Staked Ethereum
@@ -128,7 +119,6 @@ pub fn get_tokens() -> HashMap<String, Token> {
     let steth_address = "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84"
         .parse::<Address>()
         .unwrap();
-
     let steth = Token::new(steth_address, 18, String::from("STETH"), false);
 
     // Rocket Pool Ethereum
@@ -137,12 +127,11 @@ pub fn get_tokens() -> HashMap<String, Token> {
     let reth_address = "0xae78736Cd615f374D3085123A210448E74Fc6393"
         .parse::<Address>()
         .unwrap();
-
     let reth = Token::new(reth_address, 18, String::from("RETH"), false);
 
     tokens.insert("ETH".to_string(), eth);
     tokens.insert("USDC".to_string(), usdc);
-    tokens.insert("USDT".to_string(), tether);
+    tokens.insert("USDT".to_string(), usdt);
     tokens.insert("WBTC".to_string(), wbtc);
     tokens.insert("DAI".to_string(), dai);
     tokens.insert("FRAX".to_string(), frax);
