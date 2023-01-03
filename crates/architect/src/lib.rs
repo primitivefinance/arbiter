864d0067a1f4775a64865160f18a0dfd8c79c4cb
ts0yu <ts0yu@proton.me>
2023-01-03 11:26:36 +0000

    fmt

--- crates/architect/src/lib.rs
+++ crates/architect/src/lib.rs
@@ -6,10 +6,10 @@
 //! Architect is the bundling, simulation and execution module of Arbiter.
 
 use ethers::core::rand::thread_rng;
+use ethers::core::types::transaction::eip2718::TypedTransaction;
 use ethers::prelude::*;
 use ethers::signers::Signer;
 use ethers_flashbots::*;
-use ethers::core::types::transaction::eip2718::TypedTransaction;
 use url::Url;
 
 /// Type that represents an `Architect`, a transaction executor designed to
@@ -46,16 +46,24 @@ impl<S: Signer> Architect<S> {
 
         let block_number = client.get_block_number().await.unwrap();
 
-        Self { 
-            client, 
-            bundle: bundle.set_block(block_number + 1).set_simulation_block(block_number),
+        Self {
+            client,
+            bundle: bundle
+                .set_block(block_number + 1)
+                .set_simulation_block(block_number),
         }
     }
 
     /// Add and sign a transaction to the bundle to be executed.
     pub async fn add_transaction(self, transaction: TypedTransaction) {
-        let signature = self.client.signer().sign_transaction(&transaction).await.unwrap();
-        self.bundle.push_transaction(transaction.rlp_signed(&signature));
+        let signature = self
+            .client
+            .signer()
+            .sign_transaction(&transaction)
+            .await
+            .unwrap();
+        self.bundle
+            .push_transaction(transaction.rlp_signed(&signature));
     }
 
     /// Simulate bundle execution.
@@ -64,7 +72,14 @@ impl<S: Signer> Architect<S> {
     }
 
     /// Send the bundle.
-    pub async fn send(&mut self) -> ExecutionResult<PendingBundle<'_, <FlashbotsMiddleware<Provider<Http>, LocalWallet> as Middleware>::Provider>> {
+    pub async fn send(
+        &mut self,
+    ) -> ExecutionResult<
+        PendingBundle<
+            '_,
+            <FlashbotsMiddleware<Provider<Http>, LocalWallet> as Middleware>::Provider,
+        >,
+    > {
         self.client.inner().send_bundle(&self.bundle).await
     }
 }
@@ -72,8 +87,8 @@ impl<S: Signer> Architect<S> {
 #[cfg(test)]
 mod tests {
     use crate::Architect;
-    use ethers::prelude::*;
     use ethers::core::rand::thread_rng;
+    use ethers::prelude::*;
 
     // We will need more tests in future but this just ensures basic functionality is working.
     #[tokio::test]
