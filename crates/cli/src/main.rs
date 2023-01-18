c9ac95c8d5dc41081ea44d2762290b12f42d7f36
Colin Roberts <colin@primitive.xyz>
2023-01-18 11:36:23 -0700

    Trying to bypass EthersDB

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -8,10 +8,9 @@ use ethers::{
 };
 use revm::{
     db::{CacheDB, EmptyDB},
-    Database, TransactOut, TransactTo, EVM,
+    Database, TransactOut, TransactTo, EVM, AccountInfo, KECCAK_EMPTY,
 };
-use revm::db::EthersDB;
-use ethers::types::{H160, U256};
+use ethers::types::{H160, H256, U256};
 use utils::chain_tools::get_provider;
 
 #[tokio::main]
@@ -51,61 +50,72 @@ async fn main() -> Result<()> {
     // encode abi into Bytes
     let encoded = abi.encode("getReserves", ())?;
 
-    // initialize new EthersDB
-    let mut ethersdb = EthersDB::new(Arc::clone(&client), None).unwrap();
+    // initialize new db
+    let mut database = CacheDB::new(EmptyDB {});
+    let test_account = H160::from_str("0x0000000000000000000000000000000000000000")?;
+    let test_account_info = revm::AccountInfo::default();
+    // let test_account_info = revm::AccountInfo {
+    //     balance: U256::from(0),
+    //     nonce: 0,
+    //     code_hash: KECCAK_EMPTY,
+    //     code: None,
+    // };
+    database.insert_account_info(test_account, test_account_info);
+    println!("{:?}", database.accounts);
 
-    // query basic properties of an account incl bytecode
-    let acc_info = ethersdb.basic(pool_address).unwrap().unwrap();
 
-    // query value of storage slot at account address
-    let value = ethersdb.storage(pool_address, slot).unwrap();
+    // // query basic properties of an account incl bytecode
+    // let acc_info = ethersdb.basic(pool_address).unwrap().unwrap();
 
-    // initialise empty in-memory-db
-    let mut cache_db = CacheDB::new(EmptyDB::default());
+    // // query value of storage slot at account address
+    // let value = ethersdb.storage(pool_address, slot).unwrap();
 
-    // insert basic account info which was generated via Web3DB with the corresponding address
-    cache_db.insert_account_info(pool_address, acc_info);
+    // // initialise empty in-memory-db
+    // let mut cache_db = CacheDB::new(EmptyDB::default());
 
-    // insert our pre-loaded storage slot to the corresponding contract key (address) in the DB
-    cache_db
-        .insert_account_storage(pool_address, slot, value)
-        .unwrap();
+    // // insert basic account info which was generated via Web3DB with the corresponding address
+    // cache_db.insert_account_info(pool_address, acc_info);
 
-    // initialise an empty (default) EVM
-    let mut evm = EVM::new();
+    // // insert our pre-loaded storage slot to the corresponding contract key (address) in the DB
+    // cache_db
+    //     .insert_account_storage(pool_address, slot, value)
+    //     .unwrap();
 
-    // insert pre-built database from above
-    evm.database(cache_db);
+    // // initialise an empty (default) EVM
+    // let mut evm = EVM::new();
 
-    // fill in missing bits of env struc
-    // change that to whatever caller you want to be
-    evm.env.tx.caller = H160::from_str("0x0000000000000000000000000000000000000000")?;
-    // account you want to transact with
-    evm.env.tx.transact_to = TransactTo::Call(pool_address);
-    // calldata formed via abigen
-    evm.env.tx.data = Bytes::from(hex::decode(hex::encode(&encoded))?);
-    // transaction value in wei
-    evm.env.tx.value = U256::try_from(0)?;
+    // // insert pre-built database from above
+    // evm.database(cache_db);
 
-    // execute transaction without writing to the DB
-    let ref_tx = evm.transact_ref();
-    // select ExecutionResult struct
-    let result = ref_tx.0;
+    // // fill in missing bits of env struc
+    // // change that to whatever caller you want to be
+    // evm.env.tx.caller = H160::from_str("0x0000000000000000000000000000000000000000")?;
+    // // account you want to transact with
+    // evm.env.tx.transact_to = TransactTo::Call(pool_address);
+    // // calldata formed via abigen
+    // evm.env.tx.data = Bytes::from(hex::decode(hex::encode(&encoded))?);
+    // // transaction value in wei
+    // evm.env.tx.value = U256::try_from(0)?;
 
-    // unpack output call enum into raw bytes
-    let value = match result.out {
-        TransactOut::Call(value) => Some(value),
-        _ => None,
-    };
+    // // execute transaction without writing to the DB
+    // let ref_tx = evm.transact_ref();
+    // // select ExecutionResult struct
+    // let result = ref_tx.0;
 
-    // decode bytes to reserves + ts via ethers-rs's abi decode
-    let (reserve0, reserve1, ts): (u128, u128, u32) =
-        abi.decode_output("getReserves", value.unwrap())?;
+    // // unpack output call enum into raw bytes
+    // let value = match result.out {
+    //     TransactOut::Call(value) => Some(value),
+    //     _ => None,
+    // };
 
-    // Print emualted getReserves() call output
-    println!("Reserve0: {:#?}", reserve0);
-    println!("Reserve1: {:#?}", reserve1);
-    println!("Timestamp: {:#?}", ts);
+    // // decode bytes to reserves + ts via ethers-rs's abi decode
+    // let (reserve0, reserve1, ts): (u128, u128, u32) =
+    //     abi.decode_output("getReserves", value.unwrap())?;
+
+    // // Print emualted getReserves() call output
+    // println!("Reserve0: {:#?}", reserve0);
+    // println!("Reserve1: {:#?}", reserve1);
+    // println!("Timestamp: {:#?}", ts);
 
     Ok(())
 }
