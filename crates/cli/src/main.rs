96eb782cc7e87dad60c0ae5baef1df8408a08622
Colin Roberts <colin@primitive.xyz>
2023-01-24 11:21:47 -0700

    cutting loc

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -22,56 +22,53 @@ use tokio::runtime::{Handle, Runtime};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // create a testbed where we can run sims
     let mut testbed = Testbed::new();
+
+    // insert a default user
     let user_address = eH160::from_str("0x0000000000000000000000000000000000000000")?;
     let user_info = revm::AccountInfo::default();
     testbed.db.insert_account_info(user_address, user_info);
-    println!("------------CREATE USER ACCOUNT------------");
-    println!("Account created: {:#?}", testbed.db.accounts);
 
-
-    let client = get_provider().await;
-    let mut testbed = Testbed::new();
-
+    // get contract info TODO: use bindings
     let pool_address = eH160::from_str("0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852")?;
     let slot_use = 8;
     let slot = rU256::from(slot_use);
-    // Generate abi for the calldata from the human readable interface
     let abi = BaseContract::from(
         parse_abi(&[
             "function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast)",
         ])?
-    ); // TODO: this is where we can probably just use contract bindings we already have
+    ); 
 
-     // Choose block number
-     let block_number: u64 = 16434802 as u64; //.try_into().unwrap();
-     let block = Some(BlockId::from(block_number));
+    // get ethers provider
+    let client = get_provider().await;
 
-    // ----- THIS COMES FROM ethersdb.storage()
+    // Choose block number to sample data from
+    let block_number: u64 = 16434802 as u64;
+    let block = Some(BlockId::from(block_number));
+
+    // set up future and call block_on for ethers call
     let index = H256::from(slot.to_be_bytes());
         let f = async {
             let storage = client
                 .get_storage_at(pool_address, index, block)
                 .await
                 .unwrap();
-            // rU256::from_be_bytes(storage.to_fixed_bytes())
             eU256::from(storage.to_fixed_bytes())
         };
-
     let value = testbed.block_on(f);
     
     // encode abi into Bytes
     let encoded = abi.encode("getReserves", ())?;
 
     // insert our pre-loaded storage slot to the corresponding contract key (address) in the DB
-    let g = async {
+    let f = async {
         let nonce = client.get_transaction_count(pool_address, block);
         let balance = client.get_balance(pool_address, block);
         let code = client.get_code(pool_address, block);
         tokio::join!(nonce, balance, code)
     };
-    let (nonce, balance, code) = testbed.block_on(g); // EDITING RUNTIME
-        // panic on not getting data?
+    let (nonce, balance, code) = testbed.block_on(f); 
     let pool_acc_info = AccountInfo::new(
             balance.unwrap(),
             nonce
@@ -87,48 +84,23 @@ async fn main() -> Result<()> {
     testbed.db
         .insert_account_storage(pool_address, slot, value)
         .unwrap();
-    // cache_db.insert_contract();
-        // ERROR HERE? The contract doesn't actually get any bytecode?
 
-    // retrieve account from address just to check
-    // let pool_acc_info = cache_db.basic(pool_address)?.unwrap();
-    let pool_acc_info = testbed.db.basic(pool_address).unwrap().unwrap();
-
-    println!("------------CREATE POOL ACCOUNT------------");
-    println!("Pool address pulled: {:#?}", pool_acc_info);
-
-    /////////////////////////////////////////////////////////////////
-    // Initialise an EVM and perform a transaction on the Uni pool
-    /////////////////////////////////////////////////////////////////
+    // Initialise an EVM and perform a transaction
     let mut evm = EVM::new();
-
-    // insert pre-built database from above
     evm.database(testbed.db);
-    println!("---------------PRINT EVM DB---------------");
-    println!("{:#?}", evm.db().unwrap());
-    // fill in missing bits of env struc
-    // change that to whatever caller you want to be
     evm.env.tx.caller = eH160::from_str("0x0000000000000000000000000000000000000000")?;
-    // account you want to transact with
     evm.env.tx.transact_to = TransactTo::Call(pool_address);
-    // calldata formed via abigen
     evm.env.tx.data = Bytes::from(hex::decode(hex::encode(&encoded))?);
-    // transaction value in wei
     evm.env.tx.value = eU256::try_from(0)?;
-
-    // execute transaction without writing to the DB
     let ref_tx = evm.transact_ref();
-    // select ExecutionResult struct
     let result = ref_tx.0;
 
 
-    // unpack output call enum into raw bytes
+    // unpack output 
     let value = match result.out {
         TransactOut::Call(value) => Some(value),
         _ => None,
     };
-
-    // decode bytes to reserves + ts via ethers-rs's abi decode
     let (reserve0, reserve1, ts): (u128, u128, u32) =
         abi.decode_output("getReserves", value.unwrap())?;
 
@@ -138,11 +110,4 @@ async fn main() -> Result<()> {
     println!("Timestamp: {:#?}", ts);
 
     Ok(())
-}
-
-fn block_on<F: core::future::Future>(runtime: &Option<Runtime>, f: F) -> F::Output {
-    match runtime {
-        Some(runtime) => runtime.block_on(f),
-        None => futures::executor::block_on(f),
-    }
 }
