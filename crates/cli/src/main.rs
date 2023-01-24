8498f861fa58649589aa10a688f3f950635af909
Colin Roberts <colin@primitive.xyz>
2023-01-24 12:32:09 -0700

    cleaning more

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,24 +1,24 @@
-use simulate::testbed::Testbed;
 use anyhow::{Ok, Result};
 use bytes::Bytes;
-use revm::Bytecode;
-use ethers_core::k256::elliptic_curve::rand_core::block;
-use ethers_providers::Middleware;
-use ruint::aliases::{U256 as rU256, B160};
-use ethers::types::{H160 as eH160, H256, BlockId, U256 as eU256};
+use ethers::types::{BlockId, H160 as eH160, H256, U256 as eU256};
 use ethers::{
     abi::parse_abi,
     prelude::BaseContract,
     providers::{Http, Provider},
 };
+use ethers_core::k256::elliptic_curve::rand_core::block;
+use ethers_providers::Middleware;
+use revm::Bytecode;
+use ruint::aliases::{B160, U256 as rU256};
+use simulate::testbed::{self, Testbed};
 // use revm::db::EthersDB; // BROKEN IMPORT?
 use revm::{
     db::{CacheDB, EmptyDB},
     AccountInfo, Database, TransactOut, TransactTo, EVM, KECCAK_EMPTY,
 };
 use std::{str::FromStr, sync::Arc};
-use utils::chain_tools::get_provider;
 use tokio::runtime::{Handle, Runtime};
+use utils::chain_tools::get_provider;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -38,7 +38,7 @@ async fn main() -> Result<()> {
         parse_abi(&[
             "function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast)",
         ])?
-    ); 
+    );
 
     // get ethers provider
     let client = get_provider().await;
@@ -49,15 +49,15 @@ async fn main() -> Result<()> {
 
     // set up future and call block_on for ethers call
     let index = H256::from(slot.to_be_bytes());
-        let f = async {
-            let storage = client
-                .get_storage_at(pool_address, index, block)
-                .await
-                .unwrap();
-            eU256::from(storage.to_fixed_bytes())
-        };
+    let f = async {
+        let storage = client
+            .get_storage_at(pool_address, index, block)
+            .await
+            .unwrap();
+        eU256::from(storage.to_fixed_bytes())
+    };
     let value = testbed.block_on(f);
-    
+
     // encode abi into Bytes
     let encoded = abi.encode("getReserves", ())?;
 
@@ -68,35 +68,36 @@ async fn main() -> Result<()> {
         let code = client.get_code(pool_address, block);
         tokio::join!(nonce, balance, code)
     };
-    let (nonce, balance, code) = testbed.block_on(f); 
+    let (nonce, balance, code) = testbed.block_on(f);
     let pool_acc_info = AccountInfo::new(
-            balance.unwrap(),
-            nonce
-                .unwrap_or_else(|e| panic!("ethers get nonce error: {e:?}"))
-                .as_u64(),
-            Bytecode::new_raw(
-                code.unwrap_or_else(|e| panic!("ethers get code error: {e:?}"))
-                    .0,
-            ),
-        );
+        balance.unwrap(),
+        nonce
+            .unwrap_or_else(|e| panic!("ethers get nonce error: {e:?}"))
+            .as_u64(),
+        Bytecode::new_raw(
+            code.unwrap_or_else(|e| panic!("ethers get code error: {e:?}"))
+                .0,
+        ),
+    );
     testbed.db.insert_account_info(pool_address, pool_acc_info);
     let slot = eU256::from(slot_use);
-    testbed.db
+    testbed
+        .db
         .insert_account_storage(pool_address, slot, value)
         .unwrap();
 
-    // Initialise an EVM and perform a transaction
-    let mut evm = EVM::new();
-    evm.database(testbed.db);
-    evm.env.tx.caller = eH160::from_str("0x0000000000000000000000000000000000000000")?;
-    evm.env.tx.transact_to = TransactTo::Call(pool_address);
-    evm.env.tx.data = Bytes::from(hex::decode(hex::encode(&encoded))?);
-    evm.env.tx.value = eU256::try_from(0)?;
-    let ref_tx = evm.transact_ref();
+    // perform a transaction
+    // let mut tx = revm::TxEnv::default();
+    // tx = testbed.perform_transaction(user_address, pool_address, encoded);
+    testbed.evm.database(testbed.db);
+    testbed.evm.env.tx.caller = user_address;
+    testbed.evm.env.tx.transact_to = TransactTo::Call(pool_address);
+    testbed.evm.env.tx.data = Bytes::from(hex::decode(hex::encode(&encoded))?);
+    testbed.evm.env.tx.value = eU256::from(0);
+    let ref_tx = testbed.evm.transact_ref();
     let result = ref_tx.0;
 
-
-    // unpack output 
+    // unpack output
     let value = match result.out {
         TransactOut::Call(value) => Some(value),
         _ => None,
--- crates/simulate/src/testbed.rs
+++ crates/simulate/src/testbed.rs
@@ -1,12 +1,17 @@
+use std::collections::HashMap;
+
+use ethers::types::{BlockId, H160 as eH160, H256, U256 as eU256};
 use revm::{
     db::{CacheDB, DatabaseRef, EmptyDB},
-    AccountInfo, Database, Env, InMemoryDB, TransactOut, TransactTo, EVM, KECCAK_EMPTY,
+    AccountInfo, Database, Env, InMemoryDB, TransactOut, TransactTo, EVM, KECCAK_EMPTY, ExecutionResult,
 };
 use tokio::runtime::{Handle, Runtime};
+use bytes::Bytes;
 
 pub struct Testbed {
     pub db: InMemoryDB,
     pub runtime: Option<Runtime>,
+    pub evm: EVM<CacheDB<EmptyDB>>,
 }
 
 impl Testbed {
@@ -24,6 +29,7 @@ impl Testbed {
             runtime: Handle::try_current()
                 .is_err()
                 .then(|| Runtime::new().unwrap()),
+            evm: EVM::new(),
         }
     }
     pub fn block_on<F: core::future::Future>(&self, f: F) -> F::Output {
@@ -32,4 +38,15 @@ impl Testbed {
             None => futures::executor::block_on(f),
         }
     }
+
+    // pub fn perform_transaction(&self, addr_caller: eH160, addr_transact_to: eH160, data: Bytes) {
+    //     self.evm.database(self.db);
+    //     self.evm.env.tx.caller = addr_caller;
+    //     self.evm.env.tx.transact_to = TransactTo::Call(addr_transact_to);
+    //     self.evm.env.tx.data = Bytes::from(hex::decode(hex::encode(&data)));
+    //     self.evm.env.tx.value = eU256::from(0);
+    //     let ref_tx = self.evm.transact_ref();
+    // }
+
 }
+
