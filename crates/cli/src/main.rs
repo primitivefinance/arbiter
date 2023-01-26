86cac1e488e35c6675b75983637bc5c34a2956f1
Colin Roberts <colin@primitive.xyz>
2023-01-26 12:21:22 -0700

    clean up

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -137,29 +137,9 @@ async fn main() -> Result<()> {
             let user_addr = eH160::from_str("0x0000000000000000000000000000000000000000")?;
             testbed.create_user(user_addr);
 
-            // get contract info
+            // spawn a client for now
             let client = get_provider().await;
-            let block_number: u64 = 16434802_u64;
-            let block = Some(BlockId::from(block_number));
             let pool_addr = eH160::from_str("0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852")?;
-            let slot = 8;
-            let abi = BaseContract::from(
-        parse_abi(&[
-            "function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast)",
-        ])?
-    ); // TODO: USE BINDINGS INSTEAD OF USING A PROVIDER API
-            
-
-            // // set up future and call block_on for ethers call
-            // let index = H256::from(rU256::from(slot).to_be_bytes());
-            // let f = async {
-            //     let storage = client
-            //         .get_storage_at(pool_addr, index, block)
-            //         .await
-            //         .unwrap();
-            //     eU256::from(storage.to_fixed_bytes())
-            // };
-            // let value = testbed.block_on(f);
 
             let newfactory = UniswapV3Factory::new(pool_addr, client);
             let contract_call = newfactory.create_pool(eH160::from_str("0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2").unwrap(),eH160::from_str("0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2").unwrap(), 50);
@@ -167,14 +147,6 @@ async fn main() -> Result<()> {
             let bytecode = Bytecode::new_raw(Bytes::from(hex::decode(hex::encode(&contract_calldata))?));
             // println!("{:#?}", bytecode);
 
-            // // insert our pre-loaded storage slot to the corresponding contract key (address) in the DB
-            // let f = async {
-            //     let nonce = client.get_transaction_count(pool_addr, block);
-            //     let balance = client.get_balance(pool_addr, block);
-            //     let code = client.get_code(pool_addr, block);
-            //     tokio::join!(nonce, balance, code)
-            // };
-            // let (nonce, balance, code) = testbed.block_on(f);
             let pool_acc_info = AccountInfo::new(
                 eU256::from(0),
                 0,
