dc78a12e14778783bb211e438d3711c772162537
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-03-15 16:08:12 -0600

    pulled out clairvoyance

--- crates/bindings/src/lib.rs
+++ crates/bindings/src/lib.rs
@@ -4,19 +4,6 @@
 //! Do not manually edit these files.
 //! These files may be overwritten by the codegen system at any time.
 pub mod erc20;
-// pub mod i_uniswap_v3_factory;
-pub mod i_uniswap_v3_flash_callback;
-pub mod i_uniswap_v3_mint_callback;
-pub mod i_uniswap_v3_pool;
-pub mod i_uniswap_v3_pool_actions;
-pub mod i_uniswap_v3_pool_deployer;
-pub mod i_uniswap_v3_pool_derived_state;
-// pub mod i_uniswap_v3_pool_events;
-pub mod i_uniswap_v3_pool_immutables;
-pub mod i_uniswap_v3_pool_owner_actions;
-pub mod i_uniswap_v3_pool_state;
-pub mod i_uniswap_v3_swap_callback;
-pub mod uniswap_v3_factory;
 pub mod rmm01_portfolio;
 pub mod weth;
 pub mod shared_types;
