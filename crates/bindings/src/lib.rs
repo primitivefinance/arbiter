5aadc796de0f8d7a5e0c3e36d58acd1e1952c213
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-03-16 13:27:20 -0600

    removed unused bindings

--- crates/bindings/src/lib.rs
+++ crates/bindings/src/lib.rs
@@ -5,21 +5,8 @@
 //! These files may be overwritten by the codegen system at any time.
 pub mod arbiter_token;
 pub mod erc20;
-pub mod i_uniswap_v3_factory;
-pub mod i_uniswap_v3_flash_callback;
-pub mod i_uniswap_v3_mint_callback;
 pub mod i_uniswap_v3_pool;
-pub mod i_uniswap_v3_pool_actions;
-pub mod i_uniswap_v3_pool_deployer;
-pub mod i_uniswap_v3_pool_derived_state;
-pub mod i_uniswap_v3_pool_events;
-pub mod i_uniswap_v3_pool_immutables;
-pub mod i_uniswap_v3_pool_owner_actions;
-pub mod i_uniswap_v3_pool_state;
-pub mod i_uniswap_v3_swap_callback;
-pub mod meta_stable_pool;
 pub mod uniswap_v3_factory;
-pub mod vault;
 pub mod weth9;
 
 // Testing
