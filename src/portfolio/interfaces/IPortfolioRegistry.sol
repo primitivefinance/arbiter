6bfe7095cfa9bbf329c9f352bf5e4675396ee596
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-03-16 16:21:15 -0600

    Revert local contracts

--- src/portfolio/interfaces/IPortfolioRegistry.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity >=0.8.0;
-
-interface IPortfolioRegistry {
-    /**
-     * @dev Controller of the PortfolioRegistry contract.
-     */
-    function controller() external view returns (address);
-}
--- src/portfolio/interfaces/IWETH.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.5.0;
-
-interface IWETH {
-    function deposit() external payable;
-
-    function withdraw(uint256 wad) external;
-}
