7accedfff012bccb721020c48bc83c33618ffee4
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-03-16 13:57:07 -0600

    bindings

---
+++ crates/bindings/src/shared_types.rs
@@ -0,0 +1,38 @@
+///`PortfolioCurve(uint128,uint16,uint16,uint16,uint16,uint16,uint32,bool)`
+#[derive(
+    Clone,
+    ::ethers::contract::EthAbiType,
+    ::ethers::contract::EthAbiCodec,
+    Default,
+    Debug,
+    PartialEq,
+    Eq,
+    Hash
+)]
+pub struct PortfolioCurve {
+    pub max_price: u128,
+    pub jit: u16,
+    pub fee: u16,
+    pub duration: u16,
+    pub volatility: u16,
+    pub priority_fee: u16,
+    pub created_at: u32,
+    pub perpetual: bool,
+}
+///`PortfolioPair(address,uint8,address,uint8)`
+#[derive(
+    Clone,
+    ::ethers::contract::EthAbiType,
+    ::ethers::contract::EthAbiCodec,
+    Default,
+    Debug,
+    PartialEq,
+    Eq,
+    Hash
+)]
+pub struct PortfolioPair {
+    pub token_asset: ::ethers::core::types::Address,
+    pub decimals_asset: u8,
+    pub token_quote: ::ethers::core::types::Address,
+    pub decimals_quote: u8,
+}
