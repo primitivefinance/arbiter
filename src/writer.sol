2d163489f5bb5e7c1d835936cf2f0bd5394fbd30
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-03-16 13:30:17 -0600

    contracts

---
+++ src/writer.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.17 and less than 0.9.0
+pragma solidity ^0.8.17;
+
+
+/**
+ * @dev Implementation of the test interface for Arbiter writing contracts.
+ */
+contract Writer {
+    string private _test_string;
+
+    function echoString(string memory test_string) public returns (string memory) {
+        _test_string = test_string;
+        return _test_string;
+    }
+    
+}
