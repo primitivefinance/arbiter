42e670448d131a50ab82d9348df7ae5afb940c0e
Colin Roberts <colin@primitive.xyz>
2023-03-21 16:51:20 -0600

    Basic execution handler done
    I added some scaffolding for other functions and left some TODOs along the way.

--- crates/simulate/src/agent.rs
+++ crates/simulate/src/agent.rs
@@ -1,5 +1,7 @@
 use bytes::Bytes;
-use revm::primitives::{ExecutionResult, TxEnv, B160, U256};
+use revm::primitives::{ExecutionResult, Log, Output, TxEnv, B160, U256};
+
+use crate::environment::{IsDeployed, SimulationContract};
 
 // use crate::environment::SimulationEnvironment;
 
@@ -10,14 +12,39 @@ pub struct TransactSettings {
 }
 
 pub trait Agent {
-    fn call(&mut self, receiver_address: B160, call_data: Bytes, value: U256) -> ExecutionResult;
+    fn call_contract(
+        &mut self,
+        contract: &SimulationContract<IsDeployed>,
+        call_data: Bytes,
+        value: U256,
+    ) -> ExecutionResult;
     fn build_call_transaction(
         &self,
         receiver_address: B160,
         call_data: Bytes,
         value: U256,
     ) -> TxEnv;
-    fn get_logs(&mut self) -> &Vec<revm::primitives::Log>; // TODO: Not sure this needs to be mutable self
-                                                           // fn storage(&self) -> U256;
-                                                           // TODO: Should agents be labeled as `active` or `inactive` similarly to `IsDeployed` and `NotDeployed`?
+    fn read_logs(
+        &mut self,
+        contract: SimulationContract<IsDeployed>,
+        event_name: &str,
+        execution_result: ExecutionResult,
+    ) -> &Vec<revm::primitives::Log>; // TODO: Not sure this needs to be mutable self
+
+    // TODO: Should agents be labeled as `active` or `inactive` similarly to `IsDeployed` and `NotDeployed`?
+
+    // TODO: We may never actually need to return logs here depending on how we handle echoing to a buffer.
+    fn unpack_execution(&self, execution_result: ExecutionResult) -> (Bytes, Vec<Log>) {
+        // unpack output call enum into raw bytes
+        match execution_result {
+            ExecutionResult::Success { output, logs, .. } => match output {
+                Output::Call(value) => (value, logs),
+                Output::Create(_, Some(_)) => {
+                    panic!("Failed. This was a 'Create' call, use 'Deploy' instead.")
+                }
+                _ => panic!("This call has failed."),
+            },
+            _ => panic!("This call generated no execution result. This should not happen."),
+        }
+    }
 }
