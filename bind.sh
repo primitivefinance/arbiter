980b7ac62b8127e37e037323a0bdd42cd9d04871
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-03-27 17:23:54 -0600

    Test

--- bind.sh
+++ bind.sh
@@ -4,7 +4,8 @@ forge bind -c lib/arbmod/contracts -b crates/bindings/ --crate-name bindings --o
 echo "Generated bindings for arbmod" 
 forge bind -c lib/portfolio/contracts -b crates/bindings/ --crate-name bindings --overwrite --single-file
 echo "Generated bindings for portfolio"
-
+forge bind -c lib/v3-core/contracts -b crates/bindings/ --crate-name bindings --overwrite --single-file
+echo "Generated bindings for v3-core"
 #!/bin/bash
 
 # Define the input and output files
--- crates/on-chain/src/lib.rs
+++ crates/on-chain/src/lib.rs
@@ -1,2 +1,28 @@
 // pub mod executor; TODO Work on executor next
 pub mod monitor;
+
+#[cfg(test)]
+mod tests {
+    use crate::monitor::EventMonitor;
+
+    use super::*;
+    use ethers::prelude::{builders::Deployer, Signer, Wallet};
+    use ethers::providers::{Provider, Http};
+    use eyre::Result;
+    use bindings::i_uniswap_v3_pool;
+
+    // Example contract ABI and bytecode
+    const CONTRACT_ADDRESS: &str = "0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640";
+
+    #[tokio::test]
+    async fn test_event_monitor() -> Result<()> {
+        // Start a Ganache testnet
+        let event_monitor = EventMonitor::default().await;
+
+        // Create a provider with the Ganache UR
+
+        // Instantiate the EventMonitor
+        event_monitor.monitor_events(CONTRACT_ADDRESS, i_uniswap_v3_pool::IUNISWAPV3POOL_ABI.clone()).await?;
+        Ok(())
+    }
+}
