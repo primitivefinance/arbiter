d28599e59f163a23cdd770ec459638b1f0efc390
Colin Roberts <colin@primitive.xyz>
2023-03-28 09:39:29 -0600

    Bindings not in single file

---
+++ crates/bindings/src/safe_erc20.rs
@@ -0,0 +1,342 @@
+pub use safe_erc20::*;
+/// This module was auto-generated with ethers-rs Abigen.
+/// More information at: <https://github.com/gakonst/ethers-rs>
+#[allow(
+    clippy::enum_variant_names,
+    clippy::too_many_arguments,
+    clippy::upper_case_acronyms,
+    clippy::type_complexity,
+    dead_code,
+    non_camel_case_types,
+)]
+pub mod safe_erc20 {
+    #[rustfmt::skip]
+    const __ABI: &str = "[]";
+    ///The parsed JSON ABI of the contract.
+    pub static SAFEERC20_ABI: ::ethers::contract::Lazy<::ethers::core::abi::Abi> = ::ethers::contract::Lazy::new(||
+    ::ethers::core::utils::__serde_json::from_str(__ABI).expect("ABI is always valid"));
+    #[rustfmt::skip]
+    const __BYTECODE: &[u8] = &[
+        96,
+        86,
+        96,
+        55,
+        96,
+        11,
+        130,
+        130,
+        130,
+        57,
+        128,
+        81,
+        96,
+        0,
+        26,
+        96,
+        115,
+        20,
+        96,
+        42,
+        87,
+        99,
+        78,
+        72,
+        123,
+        113,
+        96,
+        224,
+        27,
+        96,
+        0,
+        82,
+        96,
+        0,
+        96,
+        4,
+        82,
+        96,
+        36,
+        96,
+        0,
+        253,
+        91,
+        48,
+        96,
+        0,
+        82,
+        96,
+        115,
+        129,
+        83,
+        130,
+        129,
+        243,
+        254,
+        115,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        48,
+        20,
+        96,
+        128,
+        96,
+        64,
+        82,
+        96,
+        0,
+        128,
+        253,
+        254,
+        162,
+        100,
+        105,
+        112,
+        102,
+        115,
+        88,
+        34,
+        18,
+        32,
+        43,
+        5,
+        185,
+        35,
+        142,
+        18,
+        95,
+        232,
+        182,
+        225,
+        118,
+        217,
+        198,
+        115,
+        244,
+        109,
+        227,
+        195,
+        239,
+        172,
+        162,
+        247,
+        116,
+        73,
+        5,
+        232,
+        220,
+        94,
+        10,
+        104,
+        125,
+        158,
+        100,
+        115,
+        111,
+        108,
+        99,
+        67,
+        0,
+        8,
+        19,
+        0,
+        51,
+    ];
+    ///The bytecode of the contract.
+    pub static SAFEERC20_BYTECODE: ::ethers::core::types::Bytes = ::ethers::core::types::Bytes::from_static(
+        __BYTECODE,
+    );
+    #[rustfmt::skip]
+    const __DEPLOYED_BYTECODE: &[u8] = &[
+        115,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        48,
+        20,
+        96,
+        128,
+        96,
+        64,
+        82,
+        96,
+        0,
+        128,
+        253,
+        254,
+        162,
+        100,
+        105,
+        112,
+        102,
+        115,
+        88,
+        34,
+        18,
+        32,
+        43,
+        5,
+        185,
+        35,
+        142,
+        18,
+        95,
+        232,
+        182,
+        225,
+        118,
+        217,
+        198,
+        115,
+        244,
+        109,
+        227,
+        195,
+        239,
+        172,
+        162,
+        247,
+        116,
+        73,
+        5,
+        232,
+        220,
+        94,
+        10,
+        104,
+        125,
+        158,
+        100,
+        115,
+        111,
+        108,
+        99,
+        67,
+        0,
+        8,
+        19,
+        0,
+        51,
+    ];
+    ///The deployed bytecode of the contract.
+    pub static SAFEERC20_DEPLOYED_BYTECODE: ::ethers::core::types::Bytes = ::ethers::core::types::Bytes::from_static(
+        __DEPLOYED_BYTECODE,
+    );
+    pub struct SafeERC20<M>(::ethers::contract::Contract<M>);
+    impl<M> ::core::clone::Clone for SafeERC20<M> {
+        fn clone(&self) -> Self {
+            Self(::core::clone::Clone::clone(&self.0))
+        }
+    }
+    impl<M> ::core::ops::Deref for SafeERC20<M> {
+        type Target = ::ethers::contract::Contract<M>;
+        fn deref(&self) -> &Self::Target {
+            &self.0
+        }
+    }
+    impl<M> ::core::ops::DerefMut for SafeERC20<M> {
+        fn deref_mut(&mut self) -> &mut Self::Target {
+            &mut self.0
+        }
+    }
+    impl<M> ::core::fmt::Debug for SafeERC20<M> {
+        fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
+            f.debug_tuple(stringify!(SafeERC20)).field(&self.address()).finish()
+        }
+    }
+    impl<M: ::ethers::providers::Middleware> SafeERC20<M> {
+        /// Creates a new contract instance with the specified `ethers` client at
+        /// `address`. The contract derefs to a `ethers::Contract` object.
+        pub fn new<T: Into<::ethers::core::types::Address>>(
+            address: T,
+            client: ::std::sync::Arc<M>,
+        ) -> Self {
+            Self(
+                ::ethers::contract::Contract::new(
+                    address.into(),
+                    SAFEERC20_ABI.clone(),
+                    client,
+                ),
+            )
+        }
+        /// Constructs the general purpose `Deployer` instance based on the provided constructor arguments and sends it.
+        /// Returns a new instance of a deployer that returns an instance of this contract after sending the transaction
+        ///
+        /// Notes:
+        /// - If there are no constructor arguments, you should pass `()` as the argument.
+        /// - The default poll duration is 7 seconds.
+        /// - The default number of confirmations is 1 block.
+        ///
+        ///
+        /// # Example
+        ///
+        /// Generate contract bindings with `abigen!` and deploy a new contract instance.
+        ///
+        /// *Note*: this requires a `bytecode` and `abi` object in the `greeter.json` artifact.
+        ///
+        /// ```ignore
+        /// # async fn deploy<M: ethers::providers::Middleware>(client: ::std::sync::Arc<M>) {
+        ///     abigen!(Greeter, "../greeter.json");
+        ///
+        ///    let greeter_contract = Greeter::deploy(client, "Hello world!".to_string()).unwrap().send().await.unwrap();
+        ///    let msg = greeter_contract.greet().call().await.unwrap();
+        /// # }
+        /// ```
+        pub fn deploy<T: ::ethers::core::abi::Tokenize>(
+            client: ::std::sync::Arc<M>,
+            constructor_args: T,
+        ) -> ::core::result::Result<
+            ::ethers::contract::builders::ContractDeployer<M, Self>,
+            ::ethers::contract::ContractError<M>,
+        > {
+            let factory = ::ethers::contract::ContractFactory::new(
+                SAFEERC20_ABI.clone(),
+                SAFEERC20_BYTECODE.clone().into(),
+                client,
+            );
+            let deployer = factory.deploy(constructor_args)?;
+            let deployer = ::ethers::contract::ContractDeployer::new(deployer);
+            Ok(deployer)
+        }
+    }
+    impl<M: ::ethers::providers::Middleware> From<::ethers::contract::Contract<M>>
+    for SafeERC20<M> {
+        fn from(contract: ::ethers::contract::Contract<M>) -> Self {
+            Self::new(contract.address(), contract.client())
+        }
+    }
+}
