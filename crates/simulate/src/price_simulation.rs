46d025c3e94b4f925f8abe8d24bc29e6abdff643
Estelle Sterrett <estellesterrett@gmail.com>
2023-04-04 22:22:55 -0400

    added struct for OU plotting

--- crates/simulate/src/price_simulation.rs
+++ crates/simulate/src/price_simulation.rs
@@ -113,6 +113,105 @@ fn generate_gbm(
     }
     price_path
 }
+/// Data needed for OU process price path generator information.
+pub struct PriceSimulationOU {
+    /// Name/identifier for the simulation (will set filenames)
+    pub identifier: String, // E.g., "test"
+    /// Numerical timestep for the simulation (typically just 1).
+    pub timestep: f64,
+    /// Time in string interpretation.
+    pub timescale: String, // E.g., "day"
+    /// Number of steps.
+    pub num_steps: usize,
+    /// Initial price of the simulation.
+    pub initial_price: f64,
+    /// Mean of the underlying asset.
+    pub mean: f64,
+    /// Volatility of the underlying asset.
+    pub volatility: f64,
+    /// Time data for the simulation.
+    pub time_data: Vec<f64>,
+    /// Price data for the simulation.
+    pub price_data: Vec<f64>,
+    /// Seed for testing.
+    pub seed: u64,
+    /// Theta for the Ornstein-Uhlenbeck process.
+    pub theta: f64,
+}
+
+
+impl PriceSimulationOU {
+    /// Public builder function that instantiates a `OU Simulation`.
+    pub fn new(
+        timestep: f64,
+        timescale: String,
+        num_steps: usize,
+        initial_price: f64,
+        mean: f64,
+        volatility: f64,
+        seed: u64, // TODO MAKE THIS OPTION
+        theta: f64,
+    ) -> Self {
+        let mut time_data: Vec<f64> = vec![];
+        for t in 0..num_steps {
+            time_data.push(t as f64 * timestep)
+        }
+        let price_data = generate_ou_process(
+            initial_price,
+            timestep,
+            mean,
+            volatility,
+            num_steps,
+            seed,
+            theta,
+        );
+
+        // Build the identifier
+        let mut identifier = String::from("timestep=");
+        identifier.push_str(&timestep.to_string());
+        identifier.push_str("_timescale=");
+        identifier.push_str(&timescale);
+        identifier.push_str("_num_steps=");
+        identifier.push_str(&num_steps.to_string());
+        identifier.push_str("_initial_price=");
+        identifier.push_str(&initial_price.to_string());
+        identifier.push_str("_mean=");
+        identifier.push_str(&mean.to_string());
+        identifier.push_str("_volatility=");
+        identifier.push_str(&volatility.to_string());
+        identifier.push_str("_seed=");
+        identifier.push_str(&seed.to_string());
+        identifier.push_str("_theta=");
+        identifier.push_str(&theta.to_string());
+
+        Self {
+            identifier,
+            timestep,
+            timescale,
+            num_steps,
+            initial_price,
+            mean,
+            volatility,
+            time_data,
+            price_data,
+            seed,
+            theta,
+        }
+    } 
+    /// Displays a plot of the OU price path.
+    
+    pub fn plot(&self) {
+        let mut filename = self.identifier.to_owned();
+        filename.push_str(".html");
+
+        let mut plot = Plot::new();
+        let trace = Scatter::new(self.time_data.clone(), self.price_data.clone());
+        plot.add_trace(trace);
+
+        plot.write_html(filename) // Produces .html using the identifier in arbiter root directory.
+    }
+}
+
 
 fn generate_ou_process(
     initial_price: f64,
@@ -136,4 +235,28 @@ fn generate_ou_process(
         price_path.push(ou);
     }
     price_path
+}
+
+fn main() {
+    let gbm = PriceSimulation::new(
+        1.0,
+        "day".to_string(),
+        100,
+        100.0,
+        0.05,
+        0.2,
+        123456789,
+    );
+    gbm.plot();
+    let ou = PriceSimulationOU::new(
+        1.0,
+        "day".to_string(),
+        100,
+        2.0,
+        1.0,
+        0.1,
+        123456789,
+        2.0,
+    );
+    ou.plot();
 }
