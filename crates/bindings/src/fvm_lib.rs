173ce3f5e6313727447918dd3c87312592068461
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-04-11 19:54:24 +0200

    Bindings with ethers-2.0.2

--- crates/bindings/src/fvm_lib.rs
+++ crates/bindings/src/fvm_lib.rs
@@ -7,16 +7,14 @@ pub use fvm_lib::*;
     clippy::upper_case_acronyms,
     clippy::type_complexity,
     dead_code,
-    non_camel_case_types
+    non_camel_case_types,
 )]
 pub mod fvm_lib {
     #[rustfmt::skip]
     const __ABI: &str = "[]";
     ///The parsed JSON ABI of the contract.
-    pub static FVMLIB_ABI: ::ethers::contract::Lazy<::ethers::core::abi::Abi> =
-        ::ethers::contract::Lazy::new(|| {
-            ::ethers::core::utils::__serde_json::from_str(__ABI).expect("ABI is always valid")
-        });
+    pub static FVMLIB_ABI: ::ethers::contract::Lazy<::ethers::core::abi::Abi> = ::ethers::contract::Lazy::new(||
+    ::ethers::core::utils::__serde_json::from_str(__ABI).expect("ABI is always valid"));
     pub struct FVMLib<M>(::ethers::contract::Contract<M>);
     impl<M> ::core::clone::Clone for FVMLib<M> {
         fn clone(&self) -> Self {
@@ -36,9 +34,7 @@ pub mod fvm_lib {
     }
     impl<M> ::core::fmt::Debug for FVMLib<M> {
         fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
-            f.debug_tuple(stringify!(FVMLib))
-                .field(&self.address())
-                .finish()
+            f.debug_tuple(stringify!(FVMLib)).field(&self.address()).finish()
         }
     }
     impl<M: ::ethers::providers::Middleware> FVMLib<M> {
@@ -48,14 +44,17 @@ pub mod fvm_lib {
             address: T,
             client: ::std::sync::Arc<M>,
         ) -> Self {
-            Self(::ethers::contract::Contract::new(
-                address.into(),
-                FVMLIB_ABI.clone(),
-                client,
-            ))
+            Self(
+                ::ethers::contract::Contract::new(
+                    address.into(),
+                    FVMLIB_ABI.clone(),
+                    client,
+                ),
+            )
         }
     }
-    impl<M: ::ethers::providers::Middleware> From<::ethers::contract::Contract<M>> for FVMLib<M> {
+    impl<M: ::ethers::providers::Middleware> From<::ethers::contract::Contract<M>>
+    for FVMLib<M> {
         fn from(contract: ::ethers::contract::Contract<M>) -> Self {
             Self::new(contract.address(), contract.client())
         }
