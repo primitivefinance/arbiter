9aedb95be6bd7468127ed3a67e99ae892eaf62c9
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-04-11 19:19:53 +0200

    lint

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -4,9 +4,12 @@
 
 use std::str::FromStr;
 
-use bindings::{rmm01_portfolio, simple_registry, uniswap_v3_pool, weth9, arbiter_token};
+use bindings::{arbiter_token, rmm01_portfolio, simple_registry, uniswap_v3_pool, weth9};
 use clap::{CommandFactory, Parser, Subcommand};
-use ethers::{abi::Tokenize, prelude::{BaseContract, H256, U256}};
+use ethers::{
+    abi::Tokenize,
+    prelude::{BaseContract, H256, U256},
+};
 use eyre::Result;
 use on_chain::monitor::EventMonitor;
 use revm::primitives::{ruint::Uint, B160};
@@ -115,12 +118,12 @@ async fn main() -> Result<()> {
                     .into_iter()
                     .collect(),
             );
-    
+
             // Choose name and symbol and combine into the constructor args required by ERC-20 contracts.
             let name = "ArbiterToken";
             let symbol = "ARBT";
             let args = (name.to_string(), symbol.to_string(), 18_u8);
-    
+
             // Call the contract deployer and receive a IsDeployed version of SimulationContract that now has an address.
             let arbiter_token = manager.agents.get("admin").unwrap().deploy(
                 &mut manager.environment,
@@ -134,24 +137,25 @@ async fn main() -> Result<()> {
 
             // Create a user to mint tokens to.
             let user_name = "arbitrageur";
-            let user_address = B160::from_str("0x0000000000000000000000000000000000000002").unwrap();
-            manager.create_user(user_address, user_name); // TODO: This should probably be done by the manager itself. THough it will be something to consider when we have more agents.
+            let user_address =
+                B160::from_str("0x0000000000000000000000000000000000000002").unwrap();
+            manager.create_user(user_address, user_name);
 
             println!("Arbitraguer created at: {}", user_address);
+
             // Allocating new tokens to user by calling Arbiter Token's ERC20 'mint' instance.
             let mint_amount = U256::from(1000);
             let input_arguments = (
                 recast_address(manager.agents[user_name].address()),
                 mint_amount,
             );
-    
             let call_data = arbiter_token
                 .base_contract
                 .encode("mint", input_arguments)
                 .unwrap()
                 .into_iter()
                 .collect();
-    
+
             // Call the 'mint' function.
             let execution_result = manager.agents.get("admin").unwrap().call_contract(
                 &mut manager.environment,
