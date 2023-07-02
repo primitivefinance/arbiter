14398c7cf884ff4d1735300874b1fd55dd59f22c
Amnot <acyclicgraphs@gmail.com>
2023-07-02 17:49:17 +0000

    apply quote crate

--- bin/init.rs
+++ bin/init.rs
@@ -35,44 +35,45 @@ pub(crate) fn create_simulation(simulation_name: &str) -> std::io::Result<()> {
         }
     }.to_string();
 
-    let startup = r#"pub(crate) fn run(manager: &mut SimulationManager) -> Result<(), Box<dyn Error>> {
-        let weth_address = manager.deployed_contracts.get("weth").unwrap().address;
-        deploy_contracts(manager, weth_address)?;
-        let liquid_exchange_xy = manager
-            .deployed_contracts
-            .get("liquid_exchange_xy")
-            .unwrap();
-        let address = B160::from_low_u64_be(2);
-        let event_filters = vec![SimulationEventFilter::new(
-            liquid_exchange_xy,
-            "PriceChange",
-        )];
-        let arbitrageur = SimpleArbitrageur::new(
-            "arbitrageur",
-            event_filters,
-            U256::from(997_000_000_000_000_000u128).into(),
-        );
-        manager
-            .activate_agent(AgentType::SimpleArbitrageur(arbitrageur), address)
-            .unwrap();
-    
-        mint(
-            &manager.deployed_contracts,
-            manager.agents.get("admin").unwrap(),
-            manager.agents.get("arbitrageur").unwrap(),
-        )?;
-        approve(
-            manager.agents.get("admin").unwrap(),
-            manager.agents.get("arbitrageur").unwrap(),
-            &manager.deployed_contracts,
-        )?;
-    
-        allocate(
-            manager.agents.get("admin").unwrap(),
-            &manager.deployed_contracts,
-        )?;
-    
-        Ok()
+    let startup = quote! {
+        pub(crate) fn run(manager: &mut SimulationManager) -> Result<(), Box<dyn Error>> {
+            let weth_address = manager.deployed_contracts.get("weth").unwrap().address;
+            deploy_contracts(manager, weth_address)?;
+            let liquid_exchange_xy = manager
+                .deployed_contracts
+                .get("liquid_exchange_xy")
+                .unwrap();
+            let address = B160::from_low_u64_be(2);
+            let event_filters = vec![SimulationEventFilter::new(
+                liquid_exchange_xy,
+                "PriceChange",
+            )];
+            let arbitrageur = SimpleArbitrageur::new(
+                "arbitrageur",
+                event_filters,
+                U256::from(997_000_000_000_000_000u128).into(),
+            );
+            manager
+                .activate_agent(AgentType::SimpleArbitrageur(arbitrageur), address)
+                .unwrap();
+
+            mint(
+                &manager.deployed_contracts,
+                manager.agents.get("admin").unwrap(),
+                manager.agents.get("arbitrageur").unwrap(),
+            )?;
+            approve(
+                manager.agents.get("admin").unwrap(),
+                manager.agents.get("arbitrageur").unwrap(),
+                &manager.deployed_contracts,
+            )?;
+
+            allocate(
+                manager.agents.get("admin").unwrap(),
+                &manager.deployed_contracts,
+            )?;
+
+            Ok(())
     }
     pub fn deploy() {
     todo!()
