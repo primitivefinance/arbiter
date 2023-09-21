1167bff1176b5b85039dd24f9eed2a06183a5ef4
Colin Roberts <colin@primitive.xyz>
2023-09-21 16:56:57 -0600

    getting closer to working

--- arbiter-core/src/data_collection.rs
+++ arbiter-core/src/data_collection.rs
@@ -1,7 +1,9 @@
 use std::{
     any::type_name,
     collections::BTreeMap,
+    env,
     fmt::Debug,
+    fs,
     marker::PhantomData,
     mem::transmute,
     sync::{atomic::AtomicBool, Arc},
@@ -10,7 +12,12 @@ use std::{
 use ethers::{
     contract::{EthEvent, EthLogDecode, Event},
     providers::{Middleware, MiddlewareError, StreamExt},
-    types::Filter,
+    types::{Filter, Log},
+};
+use futures_util::stream::select;
+use polars::{
+    prelude::{CsvWriter, DataFrame, NamedFrom, SerWriter},
+    series::Series,
 };
 use serde::Serialize;
 use serde_json::Value;
@@ -20,89 +27,120 @@ use crate::{
     middleware::{RevmMiddleware, RevmMiddlewareError},
 };
 
+enum EventCaptureItem<T> {
+    Event(T),
+    StopSignal,
+}
+
 pub type ArbiterEvent<F> = Event<Arc<RevmMiddleware>, RevmMiddleware, F>;
 
 pub struct EventCapture<F: EthEvent + EthLogDecode + Debug + 'static> {
     event: ArbiterEvent<F>,
-    capture: Vec<BTreeMap<String, Value>>,
-    pub state: Arc<AtomicBool>,
+    capture: BTreeMap<String, Vec<Value>>,
+    client: Arc<RevmMiddleware>,
+    stop_signal_sender: futures_channel::mpsc::Sender<()>,
+    stop_signal_receiver: futures_channel::mpsc::Receiver<()>,
 }
 
 impl<F: Serialize + EthEvent + EthLogDecode + Debug> EventCapture<F> {
-    pub fn new(event: ArbiterEvent<F>) -> Self {
+    pub fn new(client: Arc<RevmMiddleware>, event: ArbiterEvent<F>) -> Self {
+        let (stop_signal_sender, stop_signal_receiver) = futures_channel::mpsc::channel(1);
         Self {
             event,
-            capture: vec![],
-            state: Arc::new(AtomicBool::new(false)),
+            capture: BTreeMap::new(),
+            client,
+            stop_signal_sender,
+            stop_signal_receiver,
         }
     }
 
     // TODO: We should probalby set the state only once everything is sent to the environment properly
+    // TODO: Edit the stream so that it receives
     pub async fn run(self) -> Result<(), RevmMiddlewareError> {
-        let event_transmuter: EventTransmuter<Arc<RevmMiddleware>, RevmMiddleware, F> =
-            unsafe { transmute(self.event) };
-        let provider = event_transmuter.provider.clone();
-        let event: Event<Arc<RevmMiddleware>, RevmMiddleware, F> =
-            unsafe { transmute(event_transmuter) };
-        self.state.store(true, std::sync::atomic::Ordering::SeqCst);
+        let event_stream = Box::leak(Box::new(self.event))
+            .stream()
+            .await
+            .unwrap()
+            .map(EventCaptureItem::Event);
+
+        let mut stream = select(
+            event_stream,
+            self.stop_signal_receiver
+                .map(|_| EventCaptureItem::StopSignal),
+        );
         let mut capture = self.capture;
-        let running = self.state.clone();
-        let handle = tokio::spawn(async move {
+        println!("entering task");
+        let handle = std::thread::spawn(move || {
             println!("Listening for events");
-            let mut stream = event.stream().await.unwrap();
-            loop {
-                tokio::select! {
-                    // Check if we should stop running
-                    _ = async {
-                        if !running.load(std::sync::atomic::Ordering::SeqCst) {
-                            Some(())
-                        } else {
-                            None
-                        }
-                    } => {
-                        for (name, value) in deserialized.iter() {
-                            series_vec.push(Series::new(
-                                name,
-                                value
-                                    .as_array()
-                                    .unwrap()
-                                    .iter()
-                                    .map(|x| x.as_str().unwrap().to_string())
-                                    .collect::<Vec<String>>(),
-                            ));
-                        }
-                        let mut dataframe = DataFrame::new(series_vec)?;
+            let rt = tokio::runtime::Runtime::new().unwrap();
 
-                        // Create a directory in the CWD to store the CSV file.
-                        let current_dir = env::current_dir()?;
-                        let output_dir = current_dir.join("output");
-                        fs::create_dir_all(&output_dir)?;
-
-                        // Write out the CSV file using the environment label.
-                        let file_path = output_dir.join(format!("{}.csv", label));
-                        let file = fs::File::create(file_path)?;
-                        let mut writer = CsvWriter::new(file);
-                        writer.finish(&mut dataframe)?;
-                        break capture;
-                    }
-                    // Or continue to process events
-                    next_event = stream.next() => {
-                        if let Some(Ok(event)) = next_event {
+            rt.block_on(async {
+                while let Some(event_capture_item) = stream.next().await {
+                    match event_capture_item {
+                        EventCaptureItem::Event(Ok(event)) => {
+                            // Initialize the capture upon first event
+                            println!("got event: {:?}", event);
+                            if capture.is_empty() {
+                                let serialized = serde_json::to_string(&event).unwrap();
+                                let deserialized: BTreeMap<String, Value> =
+                                    serde_json::from_str(&serialized).unwrap();
+                                for (name, value) in deserialized.into_iter() {
+                                    capture.insert(name, vec![value]);
+                                }
+                            }
                             let serialized = serde_json::to_string(&event).unwrap();
-                            let deserialized: BTreeMap<String, Value> = serde_json::from_str(&serialized).unwrap();
-                            println!("deserialized: {:?}", deserialized);
-                            capture.push(deserialized);
+                            let deserialized: BTreeMap<String, Value> =
+                                serde_json::from_str(&serialized).unwrap();
+                            for (name, value) in deserialized.into_iter() {
+                                let storage = capture.get_mut(&name).unwrap();
+                                storage.push(value);
+                            }
+                        }
+                        EventCaptureItem::StopSignal => {
+                            println!("got stop signal");
+                            let mut series_vec = vec![];
+                            // Create a directory in the CWD to store the CSV file.
+                            let current_dir = env::current_dir().unwrap();
+                            let output_dir = current_dir.join("output");
+                            fs::create_dir_all(&output_dir).unwrap();
+
+                            if !capture.is_empty() {
+                                for (name, storage) in capture.iter() {
+                                    println!("name: {:?}", name);
+                                    println!("storage: {:?}", storage);
+                                    series_vec.push(Series::new(
+                                        name,
+                                        storage
+                                            .iter()
+                                            .map(|x| x.as_str().unwrap().to_string())
+                                            .collect::<Vec<String>>(),
+                                    ));
+                                }
+                                // Write out the CSV file using the environment label.
+                                let mut dataframe = DataFrame::new(series_vec).unwrap();
+                                let file_path = output_dir.join(format!("{}.csv", "test_label"));
+                                let file = fs::File::create(file_path).unwrap();
+                                let mut writer = CsvWriter::new(file);
+                                writer.finish(&mut dataframe).unwrap();
+                            }
+
+                            break;
+                        }
+                        EventCaptureItem::Event(Err(e)) => {
+                            println!("error: {:?}", e); //  TODO: handle properly
                         }
                     }
                 }
-            }
+            });
         });
+
+        println!("got here.");
         let instruction = Instruction::AttachCapture {
             handle,
-            state: self.state,
-            outcome_sender: provider.outcome_sender(),
+            stop_signal_sender: self.stop_signal_sender,
+            outcome_sender: self.client.outcome_sender(),
         };
-        let outcome = provider.send_instruction(instruction).await?;
+        let outcome = self.client.send_instruction(instruction).await?;
         match outcome {
             Outcome::AttachCaptureComplete => Ok(()),
             _ => Err(RevmMiddlewareError::MissingData(
--- arbiter-core/src/tests/data_output.rs
+++ arbiter-core/src/tests/data_output.rs
@@ -1,12 +1,20 @@
 use super::*;
 use crate::data_collection::EventCapture;
+use ethers::{
+    contract::{EthEvent, EthLogDecode, Event},
+    providers::{Middleware, MiddlewareError, StreamExt},
+    types::Filter,
+};
+use futures_util::SinkExt;
 
 #[tokio::test]
 async fn data_capture() {
-    let (_manager, client) = startup_user_controlled().unwrap();
+    let (mut manager, client) = startup_user_controlled().unwrap();
     let arbx = deploy_arbx(client.clone()).await.unwrap();
-    let event_capture = EventCapture::new(arbx.approval_filter());
+    let event_capture = EventCapture::new(client.clone(), arbx.approval_filter());
     event_capture.run().await.unwrap();
+    // let approval = arbx.approval_filter();
+    // let mut stream = approval.stream().await.unwrap();
 
     for _ in 0..10 {
         arbx.approve(client.address(), U256::from(1))
@@ -16,4 +24,14 @@ async fn data_capture() {
             .await
             .unwrap();
     }
+
+    // println!("next approval: {:?}", stream.next().await);
+    let environment = manager.environments.get(TEST_ENV_LABEL).unwrap();
+    let mut guard = environment.captures.lock().unwrap();
+    for (handle, mut sender) in guard.drain(..) {
+        sender.send(()).await.unwrap();
+        // handle.join();
+    }
+    println!("guard: {:?}", guard);
+    // manager.stop_environment(TEST_ENV_LABEL).unwrap();
 }
