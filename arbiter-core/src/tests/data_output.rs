cd81b6d46c113b21dce26c428684dd384139f035
Kinrezc <matt.czernik@gmail.com>
2023-09-24 14:20:02 -0400

    attempt at event capture

--- arbiter-core/src/tests/data_output.rs
+++ arbiter-core/src/tests/data_output.rs
@@ -6,24 +6,45 @@ use ethers::{
     types::Filter,
 };
 use futures_util::SinkExt;
+use tokio_util::sync::CancellationToken;
+use tokio::sync::Barrier;
 
 #[tokio::test]
 async fn data_capture() {
     let (mut manager, client) = startup_user_controlled().unwrap();
     let arbx = deploy_arbx(client.clone()).await.unwrap();
-    let event_capture = EventCapture::new(client.clone(), arbx.approval_filter());
-    event_capture.run().await.unwrap();
-    // let approval = arbx.approval_filter();
-    // let mut stream = approval.stream().await.unwrap();
+    let event_capture = EventCapture::new(arbx.events());
 
-    for _ in 0..10 {
-        arbx.approve(client.address(), U256::from(1))
-            .send()
-            .await
-            .unwrap()
-            .await
-            .unwrap();
-    }
+    let token = CancellationToken::new();
+    let token_clone = token.clone();
+
+    let handle1 = tokio::spawn(async move {
+        tokio::select! {
+            _ = token_clone.cancelled() => {
+                println!("Task 1 cancelled");
+            }
+            _ = event_capture.run() => {
+                println!("Task 1 finished");
+            }
+        }
+    });
+
+    let handle2 = tokio::spawn(async move {
+        for i in 0..1000 {
+            println!("Task 2: {}", i);
+            arbx.approve(client.address(), U256::from(1))
+                .send()
+                .await
+                .unwrap()
+                .await
+                .unwrap();
+        }
+        tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+        println!("done sleeping!");
+        token.cancel();
+    });
+    tokio::try_join!(handle1, handle2);
+
 
     // println!("next approval: {:?}", stream.next().await);
     let environment = manager.environments.get(TEST_ENV_LABEL).unwrap();
