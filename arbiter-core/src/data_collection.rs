e76c85709f81042fbe3fc524eec712896a14718d
Kinrezc <matt.czernik@gmail.com>
2023-09-24 16:34:49 -0400

    trying stuff

--- arbiter-core/src/data_collection.rs
+++ arbiter-core/src/data_collection.rs
@@ -2,16 +2,18 @@ use std::fmt::Debug;
 
 use ethers::{
     contract::{builders::Event as EventBuilder, EthLogDecode},
-    providers::{Middleware, StreamExt},
+    providers::{Middleware, StreamExt as ProviderStreamExt},
 };
 
 use futures_channel::mpsc::{Sender, Receiver, channel};
+use tokio::task::JoinHandle;
 use crate::middleware::RevmMiddlewareError;
-use std::sync::Arc;
+use stream_cancel::{StreamExt, Tripwire};
+use tracing::info;
 
 pub struct EventCapture<
-    M: Middleware + std::borrow::Borrow<D>,
-    D: Middleware + Debug + Send + Sync,
+    M: Middleware + std::borrow::Borrow<D> + 'static,
+    D: Middleware + Debug + Send + Sync + 'static,
     E: EthLogDecode + Debug + 'static,
 > {
     events: EventBuilder<M, D, E>,
@@ -27,14 +29,20 @@ impl<
         Self { events }
     }
 
-    pub async fn run(self) -> Result<(), RevmMiddlewareError> {
+    pub async fn run(self, tripwire: Tripwire) -> Result<JoinHandle<()>, RevmMiddlewareError> {
         let events = self.events;
-        println!("Events: {:?}", events);
-        let mut stream = events.stream().await.unwrap();
-        while let Some(log) = stream.next().await {
-            println!("Log: {:?}", log);
-        }
 
-        Ok(())
+        let handle = tokio::spawn(async move {
+            let mut stream = events.stream().await.unwrap().take_until_if(tripwire);
+            loop {
+                if let Some(log) = stream.next().await {
+                        info!("got inside stream!");
+                        println!("log: {:?}", log);
+                } else {
+                    continue;
+                }
+            }         
+        });
+        Ok(handle)
     }
 }
--- arbiter-core/src/tests/data_output.rs
+++ arbiter-core/src/tests/data_output.rs
@@ -6,31 +6,22 @@ use ethers::{
     types::Filter,
 };
 use futures_util::SinkExt;
-use tokio_util::sync::CancellationToken;
-use tokio::sync::Barrier;
+use stream_cancel::{StreamExt as StreamCancelExt, Tripwire};
+use tracing::info;
+use tracing_test::traced_test;
 
+#[traced_test]
 #[tokio::test]
 async fn data_capture() {
     let (mut manager, client) = startup_user_controlled().unwrap();
     let arbx = deploy_arbx(client.clone()).await.unwrap();
     let event_capture = EventCapture::new(arbx.events());
 
-    let token = CancellationToken::new();
-    let token_clone = token.clone();
+    let (trigger, tripwire) = Tripwire::new();
 
-    let handle1 = tokio::spawn(async move {
-        tokio::select! {
-            _ = token_clone.cancelled() => {
-                println!("Task 1 cancelled");
-            }
-            _ = event_capture.run() => {
-                println!("Task 1 finished");
-            }
-        }
-    });
 
     let handle2 = tokio::spawn(async move {
-        for i in 0..1000 {
+        for i in 0..10 {
             println!("Task 2: {}", i);
             arbx.approve(client.address(), U256::from(1))
                 .send()
@@ -39,11 +30,10 @@ async fn data_capture() {
                 .await
                 .unwrap();
         }
-        tokio::time::sleep(std::time::Duration::from_secs(5)).await;
-        println!("done sleeping!");
-        token.cancel();
+        info!("Task 2: done");
+        drop(trigger);
     });
-    tokio::try_join!(handle1, handle2);
+    tokio::join!(event_capture.run(tripwire), handle2);
 
 
     // println!("next approval: {:?}", stream.next().await);
