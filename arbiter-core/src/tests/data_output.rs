c1c5a8794b9f6d70e4828da56f8e7c3a5ec68d7b
Kinrezc <matt.czernik@gmail.com>
2023-09-27 19:05:33 -0400

    working logger test

--- arbiter-core/src/tests/data_output.rs
+++ arbiter-core/src/tests/data_output.rs
@@ -13,22 +13,24 @@ use tracing_test::traced_test;
 #[traced_test]
 #[tokio::test]
 async fn data_capture() {
-    let (mut manager, client) = startup_user_controlled().unwrap();
+    let (mut _manager, client) = startup_user_controlled().unwrap();
     let arbx = deploy_arbx(client.clone()).await.unwrap();
-    let event_capture = EventCapture::new(arbx.events());
+    let event_capture = EventCapture::new(arbx.events(), client.clone());
 
-    let (handle1, tx) = event_capture.run().await.unwrap();
-
-    for i in 0..5 {
-        info!("Task 1: {}", i);
-        arbx.approve(client.address(), U256::from(1))
-            .send()
-            .await
-            .unwrap()
-            .await
-            .unwrap();
-    }
-    info!("Task 2: done");
-    tx.send(()).unwrap();
-    handle1.join();
+    let (tx, handle) = event_capture.run().await.unwrap();
+    
+    let handle2 = tokio::spawn(async move {
+        for i in 0..5 {
+            info!("Task 1: {}", i);
+            arbx.approve(client.address(), U256::from(1))
+                .send()
+                .await
+                .unwrap()
+                .await
+                .unwrap();
+        }
+        tx.send(()).unwrap();
+        info!("Task 2: done");
+    });
+    tokio::join!(handle, handle2);
 }
