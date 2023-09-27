e8ef7ab5f5326f196c9a560e343c8813f274f62f
Kinrezc <matt.czernik@gmail.com>
2023-09-27 19:05:33 -0400

    working logger test

--- arbiter-core/src/data_collection.rs
+++ arbiter-core/src/data_collection.rs
@@ -1,12 +1,13 @@
-use std::fmt::Debug;
+use std::{fmt::Debug, sync::Arc};
 
 use ethers::{
     contract::{builders::Event as EventBuilder, EthLogDecode},
-    providers::{Middleware, StreamExt as ProviderStreamExt},
+    providers::{Middleware, StreamExt as ProviderStreamExt}, types::{Filter, U256},
+    prelude::k256::sha2::{Digest, Sha256}
 };
 use futures_util::FutureExt;
 
-use crate::middleware::RevmMiddlewareError;
+use crate::middleware::{RevmMiddlewareError, RevmMiddleware};
 use tokio::sync::oneshot::{Receiver, Sender};
 use stream_cancel::{StreamExt, Tripwire};
 use tokio::task::JoinHandle;
@@ -19,6 +20,8 @@ pub struct EventCapture<
 > {
     events: EventBuilder<M, D, E>,
     channel: (Sender<()>, Receiver<()>),
+    id: U256,
+    client: Arc<RevmMiddleware>
 }
 
 impl<
@@ -27,39 +30,51 @@ impl<
         E: EthLogDecode + Debug + 'static,
     > EventCapture<M, D, E>
 {
-    pub fn new(events: EventBuilder<M, D, E>) -> Self {
-        Self { events, channel: tokio::sync::oneshot::channel() }
+    pub fn new(events: EventBuilder<M, D, E>, client: Arc<RevmMiddleware>) -> Self {
+        let filter = events.filter.clone();
+        let mut hasher = Sha256::new();
+        hasher.update(serde_json::to_string(&filter).map_err(RevmMiddlewareError::Json).unwrap());
+        let hash = hasher.finalize();
+        let id = ethers::types::U256::from(ethers::types::H256::from_slice(&hash).as_bytes());
+        Self { events, channel: tokio::sync::oneshot::channel(), id, client }
     }
 
     pub async fn run(
         self,
-    ) -> Result<(std::thread::JoinHandle<()>, Sender<()>), RevmMiddlewareError> {
+    ) -> Result<(Sender<()>, JoinHandle<()>), RevmMiddlewareError> {
         let events = self.events;
-        let (sender, mut receiver) = self.channel;
-        let handle = std::thread::spawn(move || {
-            let rt = tokio::runtime::Runtime::new().unwrap();
-            rt.block_on(async {
-                let mut stream = events.stream().await.unwrap();
-                loop {
-                    info!("looping");
-                    tokio::select! {
-                        biased;
-                        Some(log) = stream.next() => {
-                            info!("log: {:?}", log);
+        let (sender, mut recv) = self.channel;
+        let handle = tokio::spawn(async move {
+            let client = self.client.clone();
+            let connection = client.provider().as_ref();
+            let mut stream = events.stream().await.unwrap();
+            info!("here1");
+            loop {
+                info!("looping");
+                tokio::select! {
+                    Some(log) = stream.next() => {
+                        info!("log: {:?}", log);
+                    }
+                    _ = &mut recv => {
+                        info!("received message");
+                        loop {
+                            let receivers = connection.filter_receivers.lock().await;
+                            if let Some (receiver) = receivers.get(&self.id) {
+                                if receiver.receiver.len() == 0 {
+                                    break;
+                                } else {
+                                    drop(receivers);
+                                    if let Some(log) = stream.next().await {
+                                        info!("log: {:?}", log);
+                                    }
+                                }
+                            }
                         }
-                        _ = &mut receiver => {
-                            info!("received message");
-                            let remainder = stream.next().now_or_never();
-                            info!("remainder: {:?}", remainder);
-                            break;
-                        }
+                        break;
                     }
                 }
-                info!("exiting");
-                let remainder = stream.next().now_or_never();
-                info!("remainder: {:?}", remainder);
-            });
+            }
         });
-        Ok((handle, sender))
+        Ok((sender, handle))
     }
 }
