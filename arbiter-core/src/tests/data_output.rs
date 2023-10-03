cdec16b52cd38fbb5d5973f1372e9e1abc625219
Kinrezc <matt.czernik@gmail.com>
2023-10-03 11:55:24 -0400

    add test for file creation

--- arbiter-core/src/tests/data_output.rs
+++ arbiter-core/src/tests/data_output.rs
@@ -2,6 +2,7 @@ use super::*;
 use crate::data_collection::EventLogger;
 use tracing::info;
 use tracing_test::traced_test;
+use tokio::io::AsyncReadExt;
 
 #[traced_test]
 #[tokio::test(flavor = "multi_thread")]
@@ -15,8 +16,7 @@ async fn data_capture() {
 
     listener.run().await.unwrap();
 
-    for i in 0..5 {
-        info!("Task 1: {}", i);
+    for _ in 0..5 {
         arbx.approve(client.address(), U256::from(1))
             .send()
             .await
@@ -30,11 +30,14 @@ async fn data_capture() {
             .await
             .unwrap();
     }
+    // check if dir exists
+    tokio::fs::remove_dir_all("./events").await.unwrap();
 }
+
 
 #[traced_test]
 #[tokio::test(flavor = "multi_thread")]
-async fn data_capture_with_path() {
+async fn data_capture_output_validation() {
     let (mut _manager, client) = startup_user_controlled().unwrap();
     let arbx = deploy_arbx(client.clone()).await.unwrap();
     let arby = deploy_arbx(client.clone()).await.unwrap();
@@ -45,8 +48,7 @@ async fn data_capture_with_path() {
 
     listener.run().await.unwrap();
 
-    for i in 0..5 {
-        info!("Task 1: {}", i);
+    for _ in 0..5 {
         arbx.approve(client.address(), U256::from(1))
             .send()
             .await
@@ -60,4 +62,25 @@ async fn data_capture_with_path() {
             .await
             .unwrap();
     }
+
+    let mut file0 = tokio::fs::File::open("./test_output/arbx/ApprovalFilter.csv")
+        .await
+        .unwrap();
+    let mut contents0 = vec![];
+    file0.read_to_end(&mut contents0).await.unwrap();
+    let contents0 = String::from_utf8(contents0).unwrap();
+
+    let mut file1 = tokio::fs::File::open("./test_output/arby/ApprovalFilter.csv")
+        .await
+        .unwrap();
+    let mut contents1 = vec![];
+    file1.read_to_end(&mut contents1).await.unwrap();
+    let contents1 = String::from_utf8(contents1).unwrap();
+
+    assert_eq!(contents0, contents1);
+
+    // deleta test_output directory
+
+    tokio::fs::remove_dir_all("./test_output").await.unwrap();
+
 }
