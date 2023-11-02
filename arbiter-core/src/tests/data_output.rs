5a39fde5f73a74d805c029094954ba4d2c83cfec
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-11-02 08:38:35 -0600

    added data output test back

---
+++ arbiter-core/src/tests/data_output.rs
@@ -0,0 +1,43 @@
+use tracing_test::traced_test;
+
+use super::*;
+use crate::data_collection::EventLogger;
+
+#[traced_test]
+#[tokio::test(flavor = "multi_thread")]
+async fn data_capture() {
+    let (env, client) = startup_user_controlled().unwrap();
+    let (arbx, arby, lex) = deploy_liquid_exchange(client.clone()).await.unwrap();
+    println!("Deployed contracts");
+
+    let listener = EventLogger::builder()
+        .add(arbx.events(), "arbx")
+        .add(arby.events(), "arby")
+        .add(lex.events(), "lex");
+
+    listener.run().unwrap();
+
+    for _ in 0..5 {
+        arbx.approve(client.address(), U256::from(1))
+            .send()
+            .await
+            .unwrap()
+            .await
+            .unwrap();
+        arby.approve(client.address(), U256::from(1))
+            .send()
+            .await
+            .unwrap()
+            .await
+            .unwrap();
+        lex.set_price(U256::from(10u128.pow(18)))
+            .send()
+            .await
+            .unwrap()
+            .await
+            .unwrap();
+    }
+
+    let _ = env.stop();
+    tokio::fs::remove_dir_all("./data").await.unwrap();
+}
