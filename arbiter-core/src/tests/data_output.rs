91a373a445ea8a147fe9999cc7f5dec82efae232
Kinrezc <matt.czernik@gmail.com>
2023-11-04 19:29:19 -0400

    consolidate tests into one function and wait 1 second for race condition on file creation

--- arbiter-core/src/tests/data_output.rs
+++ arbiter-core/src/tests/data_output.rs
@@ -9,43 +9,14 @@ async fn data_capture() {
     let (arbx, arby, lex) = deploy_liquid_exchange(client.clone()).await.unwrap();
     println!("Deployed contracts");
 
-    let listener = EventLogger::builder()
+    // default_listener
+   EventLogger::builder()
         .add(arbx.events(), "arbx")
         .add(arby.events(), "arby")
-        .add(lex.events(), "lex");
-
-    listener.run().unwrap();
-
-    for _ in 0..5 {
-        arbx.approve(client.address(), U256::from(1))
-            .send()
-            .await
-            .unwrap()
-            .await
-            .unwrap();
-        arby.approve(client.address(), U256::from(1))
-            .send()
-            .await
-            .unwrap()
-            .await
-            .unwrap();
-        lex.set_price(U256::from(10u128.pow(18)))
-            .send()
-            .await
-            .unwrap()
-            .await
-            .unwrap();
-    }
-
-    let _ = env.stop();
-    // std::fs::remove_dir_all("./data").unwrap();
-}
-
-#[tokio::test]
-async fn csv_output() {
-    let (env, client) = startup_user_controlled().unwrap();
-    let (arbx, arby, lex) = deploy_liquid_exchange(client.clone()).await.unwrap();
-
+        .add(lex.events(), "lex")
+        .run()
+        .unwrap();
+    
     EventLogger::builder()
         .add(arbx.events(), "arbx")
         .add(arby.events(), "arby")
@@ -53,21 +24,6 @@ async fn csv_output() {
         .file_type(OutputFileType::CSV)
         .run()
         .unwrap();
-
-    // Perform some operations that generate events...
-    generate_events(arbx, arby, lex, client.clone()).await.unwrap_or_else(|e| {
-        panic!("Error generating events: {}", e);
-    });
-
-    let _ = env.stop();
-    assert!(Path::new("./data/output.csv").exists());
-    // Check if the CSV file was created
-}
-
-#[tokio::test]
-async fn parquet_output() {
-    let (env, client) = startup_user_controlled().unwrap();
-    let (arbx, arby, lex) = deploy_liquid_exchange(client.clone()).await.unwrap();
 
     EventLogger::builder()
         .add(arbx.events(), "arbx")
@@ -77,14 +33,17 @@ async fn parquet_output() {
         .run()
         .unwrap();
 
-    // Perform some operations that generate events...
     generate_events(arbx, arby, lex, client.clone()).await.unwrap_or_else(|e| {
         panic!("Error generating events: {}", e);
     });
 
     let _ = env.stop();
 
+    std::thread::sleep(std::time::Duration::from_secs(1));
+    assert!(Path::new("./data/output.csv").exists());
     assert!(Path::new("./data/output.parquet").exists());
+    assert!(Path::new("./data/output.json").exists());
+    std::fs::remove_dir_all("./data").unwrap();
 }
 
 async fn generate_events(arbx: ArbiterToken<RevmMiddleware>, arby: ArbiterToken<RevmMiddleware>, lex: LiquidExchange<RevmMiddleware>, client: Arc<RevmMiddleware>) -> Result<(), RevmMiddlewareError>{
