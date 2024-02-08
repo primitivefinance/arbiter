77795926c06208cc82d04c72bdd1b65cdf2974e2
Colin Roberts <colin@primitive.xyz>
2024-02-08 10:04:10 -0700

    deps cleanup

--- arbiter-core/tests/common.rs
+++ arbiter-core/tests/common.rs
@@ -1,6 +1,5 @@
 use std::sync::Arc;
 
-use anyhow::Result;
 use arbiter_bindings::bindings::{
     arbiter_math::ArbiterMath, arbiter_token::ArbiterToken, liquid_exchange::LiquidExchange,
 };
@@ -28,60 +27,69 @@ pub const ARBITER_TOKEN_Y_DECIMALS: u8 = 18;
 
 pub const LIQUID_EXCHANGE_PRICE: f64 = 420.69;
 
-pub fn startup() -> Result<(Environment, Arc<ArbiterMiddleware>)> {
-    let env = Environment::builder().build();
-    let client = ArbiterMiddleware::new(&env, Some(TEST_SIGNER_SEED_AND_LABEL))?;
-    Ok((env, client))
+pub fn log() {
+    std::env::set_var("RUST_LOG", "trace");
+    tracing_subscriber::fmt::init();
 }
 
-pub async fn deploy_arbx(
-    client: Arc<ArbiterMiddleware>,
-) -> Result<ArbiterToken<ArbiterMiddleware>> {
-    Ok(ArbiterToken::deploy(
+pub fn startup() -> (Environment, Arc<ArbiterMiddleware>) {
+    let env = Environment::builder().build();
+    let client = ArbiterMiddleware::new(&env, Some(TEST_SIGNER_SEED_AND_LABEL)).unwrap();
+    (env, client)
+}
+
+pub async fn deploy_arbx(client: Arc<ArbiterMiddleware>) -> ArbiterToken<ArbiterMiddleware> {
+    ArbiterToken::deploy(
         client,
         (
             ARBITER_TOKEN_X_NAME.to_string(),
             ARBITER_TOKEN_X_SYMBOL.to_string(),
             ARBITER_TOKEN_X_DECIMALS,
         ),
-    )?
+    )
+    .unwrap()
     .send()
-    .await?)
+    .await
+    .unwrap()
 }
 
-pub async fn deploy_arby(
-    client: Arc<ArbiterMiddleware>,
-) -> Result<ArbiterToken<ArbiterMiddleware>> {
-    Ok(ArbiterToken::deploy(
+pub async fn deploy_arby(client: Arc<ArbiterMiddleware>) -> ArbiterToken<ArbiterMiddleware> {
+    ArbiterToken::deploy(
         client,
         (
             ARBITER_TOKEN_Y_NAME.to_string(),
             ARBITER_TOKEN_Y_SYMBOL.to_string(),
             ARBITER_TOKEN_Y_DECIMALS,
         ),
-    )?
+    )
+    .unwrap()
     .send()
-    .await?)
+    .await
+    .unwrap()
 }
 
 pub async fn deploy_liquid_exchange(
     client: Arc<ArbiterMiddleware>,
-) -> Result<(
+) -> (
     ArbiterToken<ArbiterMiddleware>,
     ArbiterToken<ArbiterMiddleware>,
     LiquidExchange<ArbiterMiddleware>,
-)> {
-    let arbx = deploy_arbx(client.clone()).await?;
-    let arby = deploy_arby(client.clone()).await?;
+) {
+    let arbx = deploy_arbx(client.clone()).await;
+    let arby = deploy_arby(client.clone()).await;
     let price = parse_ether(LIQUID_EXCHANGE_PRICE).unwrap();
-    let liquid_exchange = LiquidExchange::deploy(client, (arbx.address(), arby.address(), price))?
+    let liquid_exchange = LiquidExchange::deploy(client, (arbx.address(), arby.address(), price))
+        .unwrap()
         .send()
-        .await?;
-    Ok((arbx, arby, liquid_exchange))
+        .await
+        .unwrap();
+    (arbx, arby, liquid_exchange)
 }
 
-pub async fn deploy_arbiter_math(
-    client: Arc<ArbiterMiddleware>,
-) -> Result<ArbiterMath<ArbiterMiddleware>> {
-    Ok(ArbiterMath::deploy(client, ())?.send().await?)
+pub async fn deploy_arbiter_math(client: Arc<ArbiterMiddleware>) -> ArbiterMath<ArbiterMiddleware> {
+    ArbiterMath::deploy(client, ())
+        .unwrap()
+        .send()
+        .await
+        .unwrap()
 }
