360f4a7f38667e761f8306ec140418eceab1bccd
Colin Roberts <colin@primitive.xyz>
2024-04-23 14:20:08 -0600

    fix: generics on `State` macro

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -18,9 +18,12 @@ use syn::{
 pub fn derive_state(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = &input.ident;
+    let generics = &input.generics;
+
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
     let expanded = quote! {
-        impl State for #name {
+        impl #impl_generics State for #name #ty_generics #where_clause {
             type Data = Self;
         }
     };
